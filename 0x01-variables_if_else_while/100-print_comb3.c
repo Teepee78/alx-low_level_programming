a23b4da115126f537fe74d86132ccf1aefb728be
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-08 05:28:56 -0700

    Fix 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 	for (digit1 = 48; digit1 < 58; digit1++)
 	{
-		for (digit2 = 48; digit2 < 58; digit2++)
+		for (digit2 = digit1; digit2 < 58; digit2++)
 		{
 			if (digit1 != digit2)
 			{
 				putchar(digit1);
 				putchar(digit2);
-				if (digit1 == 57 && digit2 == 56)
+				if (digit1 == 56 && digit2 == 57)
 				{
 					continue;
 				} else
