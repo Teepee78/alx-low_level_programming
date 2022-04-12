9f8846a6b35f6d216687cc4ed397eac393a8f66a
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-12 14:14:20 -0700

    Create 100-times_table.c, update README.md and main.h

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints n time table
+ * @n: integer
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int i, j, result;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (n < 0 || n > 15)
+			{
+				break;
+			} else
+			{
+				result = i * j;
+
+				if (i = n - 1 && j = n - 1)
+				{
+					if (result < 0)
+					{
+						_putchar(result + '0');
+						_putchar('\n');
+					} else
+					{
+						if (j = n - 1)
+						{
+							_putchar(result/10 + '0');
+							_putchar(result%10 + '0');
+							_putchar('\n');
+						} else
+						{
+							_putchar(result/10 + '0');
+							_putchar(result%10 + '0');
+							_putchar(',');
+							_putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+}
