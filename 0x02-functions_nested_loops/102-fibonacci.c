e836ca40223ed5025998abc9571e4f009a422118
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-12 15:03:41 -0700

    Add return statements to 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,21 +3,26 @@
 /**
  * fibonacci - prints fibonacci numbers
  * @n: integer
+ *
+ * Return: result
  */
-void fibonacci(int n)
+int fibonacci(int n)
 {
 	int result;
 
 	if (n == 1)
 	{
 		printf("1");
+		return (1);
 	} else if (n == 2)
 	{
 		printf("2");
+		return (2);
 	} else if (n > 0)
 	{
 		result = fibonacci(n - 1) + fibonacci(n - 2);
 		printf("%d\n", result);
+		return (result);
 	}
 }
 /**
