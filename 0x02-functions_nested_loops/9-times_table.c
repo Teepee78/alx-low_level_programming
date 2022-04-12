e77dc4279e0006727e757e609b96ddf6d73fc1c2
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-12 08:17:39 -0700

    Edit 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,15 +17,31 @@ void times_table(void)
 
 			result = i * j;
 
-			if (j == 9)
+			if ((result / 10) == 0)
 			{
-				_putchar(result + '0');
-				_putchar('\n');
+				if (j == 0)
+				{
+					_putchar ('0');
+				}
+				if (j != 0)
+				{
+					_putchar (' ');
+					_putchar ((result % 10) + '0');
+				}
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			} else
 			{
-				_putchar(result + '0');
-				_putchar(',');
-				_putchar(' ');
+				_putchar ((result / 10) + '0');
+				_putchar ((result % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar (' ');
+				}
 			}
 		}
 	}
