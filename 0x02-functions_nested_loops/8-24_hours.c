9febfe33303ecb9c8aff131ebe521e8da9e79d53
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-12 07:21:10 -0700

    Create 8-24_hours.c, update README.md and main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int i , j, k, l;
+
+	for (i = 48; i < 51; i++)
+	{
+
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 54; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+				}
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -5,4 +5,5 @@
 4. 4-isalpha.c checks if a character is alphabetic
 5. 5-sign.c prints the sign of a number
 6. 6-abs.c returns the absolute number of a given integer
-7. 7-print_last_digit.c returns the last digit of a given number
+7. 7-print_last_digit.c returns the last digit of a given numbeir
+8. 8-24_hours.c prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
