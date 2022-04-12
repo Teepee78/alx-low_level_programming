7ae43896c96f6f32c8547b69b911b225d9f56c6b
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-12 09:15:49 -0700

    edit 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,6 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-
 		for (i = n; i <= 98; i++)
 		{
 			if (i < 98)
@@ -31,9 +30,8 @@ void print_to_98(int n)
 		{
 			if (i > 98)
 			{
-
 				printf("%d, ", i);
-			} else 
+			} else
 			{
 				printf("%d\n", i);
 			}
