5ed527a6f0ff6db26cbd2ef77df0b9ec954b5a9b
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-14 13:57:19 -0700

    Create 3-print_numbers.c, edit main.h, README.md

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0. 0-isupper.c checks if a character is uppercase
 1. 1-isdigit.c checks if a character is a digit
 2. 2-mul.c multiplies two integers
+3. 3-print_numbers.c print 0-9
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,12 @@
 #include <ctype.h>
 #include <stdio.h>
 
+int _putchar(char c);
+
 int _isupper(int c);
 
 int _isdigit(int c);
 
 int mul(int a, int b);
+
+void print_numbers(void);
