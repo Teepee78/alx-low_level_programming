dbee901260c18490b3df59f903e2a84e59f263bc
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-14 15:09:43 -0700

    Create 6-print_line.c, edit main.h, README.md

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number of times '_' should be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		n = 0;
+	}
+
+	i = 0;
+	while (i < n)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -4,3 +4,4 @@
 3. 3-print_numbers.c print 0-9
 4. 4-print_most_numbers.c prints 0-9, except 2 and 4
 5. void more_numbers(void) prints 10x o - 14
+6. 6-print_line.c draws a straight line
