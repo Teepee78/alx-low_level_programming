8264b918cd5b8fa0413c927b030e214082848c7b
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-14 13:33:28 -0700

    Create 0-isupper.c, main.h, README.md

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, else 0
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int _isupper(int c);
