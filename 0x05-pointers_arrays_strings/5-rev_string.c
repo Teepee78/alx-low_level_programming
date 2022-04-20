5db5cc1cb683e1b490ffc050a116c6ac853d6f75
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-20 03:12:54 -0700

    Create 5-rev_string.c, update main.h and README.md

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	char new_s[];
+
+	i = strlen(s) - 1;
+	j = 0
+	while (i >= 0)
+	{
+		new_s[j] = s[i];
+		j++;
+		i--;
+	}
+
+	sizenew_s = sizeof(new_s);
+	sizeint = sizeof(int);
+	lennew_s = sizenew_s / sizeint;
+
+	new_s[lennew_s] = '\0';
+	*s = new_s;
+}
