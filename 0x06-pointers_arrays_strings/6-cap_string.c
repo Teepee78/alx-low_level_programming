d67ec3390d8c199cbf9bb85d4c65b30cedcb3b76
Teepee <eniolatoluwalase78@gmail.com>
2022-04-22 15:00:12 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *s)
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 	{
 		*(s + count) = *(s + count) - 32;
- 		count++;
+		count++;
 	}
 
 	while (*(s + count) != '\0')
@@ -25,7 +25,7 @@ char *cap_string(char *s)
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
 				{
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
- 					break;
+					break;
 				}
 			}
 		}
