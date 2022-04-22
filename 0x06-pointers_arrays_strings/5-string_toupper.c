f6c6df119156f96d7b95e6f5bf03d2cba114a342
Teepee78 <eniolatoluwalase78@gmail.com>
2022-04-22 05:37:37 -0700

    Create 5-string_toupper.c, update main.h and README.md

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a string is lowercase
+ * @a: string
+ * Return: True or False
+ */
+char _islower(char a)
+{
+	int i = a;
+
+	return (96 < i && i < 123);
+}
+
+char *string_toupper(char *str)
+{
+	int i, j, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (_islower(str[i]))
+		{
+			j = str[i];
+
+			str[i] = j - 32;
+		}
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@
 2. 2-strncpy.c copies a string
 3. 3-strcmp.c compares 2 strings
 4. 4-rev_array.c reverses an array of integers
+5. 5-string_toupper.c changes uppercase letters to lowercase
