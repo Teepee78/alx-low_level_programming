54f4f4d18117831c78fdf60494f330b282a0bfc2
Teepee <eniolatoluwalase78@gmail.com>
2022-04-26 13:06:09 +0100

    Create 4-strpbrk.c, update main.h and README.md

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for a set of bytes
+ * @s: string
+ * @accept: substring
+ * Return: return s if it matches accept, 0 if not
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '0')
+	{
+		while (*accept != '0')
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+		}
+
+		if (*s == *accept)
+		{
+			return (s);
+		}
+	}
+	return (0);
+}
