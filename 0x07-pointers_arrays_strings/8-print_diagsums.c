f4e8a91faf4c5153a424a2fd563efed8f53c7e75
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-04-26 15:11:27 +0100

    Create 8-print_diagsums.c,update main.h and READMe.md

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * printdiagsums - prints the sum of a diagonal
+ * @a: matrix
+ * @size: size of a
+ */
+void print_diagsums(int *a, int size)
+{
+	/* declare variables*/
+	int i;
+	int sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	i = 0;
+	while (i < size) /*number repetitions*/
+	{
+		/*sums for diagsums*/
+		sum1 = sum1 + *(a + i * size + i);
+		sum2 = sum2 + *(a + i * size + size - i - 1);
+
+		i++;
+	}
+
+	printf("%i, %i\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,3 +5,4 @@
 4. 4-strpbrk.c searches a string for a set of bytes
 5. 5-strstr.c locates a substring.
 6. 7-print_chessboard.c prints the chessboard.
+7. 8-print_diagsums.c prints the sum of two diagonals of a square matrix of integers.
