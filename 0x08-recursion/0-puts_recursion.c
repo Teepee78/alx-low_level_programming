996377e49fa7ebdd76ed0bb1927962bf5c21b6e5
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-04-28 12:18:06 +0100

    Update 0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,10 +6,10 @@
  */
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
-	_putchar(s);
-	_putchar(_puts_recursion(s++));
+	_putchar(*s);
+	_putchar(_puts_recursion(*(s)++));
 }
