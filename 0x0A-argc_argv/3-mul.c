5ceca0dffd22375646528c1f3205ee80c13c3fc4
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-02 11:22:30 +0100

    Create 3-mul.c, update README.md

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 (success), 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int total, num1, num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		total = num1 * num2;
+
+		printf("%d\n", total);
+		return (0);
+	} else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 0. 0-whatsmyname.c prints it's name, followed by a new line
 1. 1-args.c prints the number of arguments passed into it
 2. 2-args.c prints all arguments it receives
+3. 3-mul.c multiplies 2 numbers
