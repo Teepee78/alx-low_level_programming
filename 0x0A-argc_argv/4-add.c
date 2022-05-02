27bc50c0b55cef78a4f09ffff77fe124d9d1bc51
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-02 11:41:50 +0100

    Create 4-add.c, update README.md

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return; Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Entry point
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 (success), 1 (error)
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum, num;
+
+	sum = 0;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (check_num(argv[i]))
+		{
+			num = atoi(argv[i]);
+			sum += num;
+		} else
+		{
+			printf("Eror\n");
+			return (1);
+		}
+
+		i++;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,4 @@
 1. 1-args.c prints the number of arguments passed into it
 2. 2-args.c prints all arguments it receives
 3. 3-mul.c multiplies 2 numbers
+4. 4-add.c adds positive numbers
