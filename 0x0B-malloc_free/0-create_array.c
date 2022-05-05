8c2884665a539a7aeb82595f51ccb1b15407ebd9
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-05 20:31:19 +0100

    Create 0-create_array.c, README.md, main.h

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and initializes it with a specific character
+ * @size: size of array
+ * @c: character
+ * Return: array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+
+	if (size == NULL)
+		return (0);
+
+	array = malloc(sizeof(array) * size);
+	array[0] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+0. 0-create_array.c creates an array and initializes it with a specific character
