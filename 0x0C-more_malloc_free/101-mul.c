412919f3538e6fa773762d5391dcfd2ea5ff9d0a
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-09 11:27:29 +0100

    Update

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if a string contains only digits
+ * @str: string
+ * Return: 1 (success)
+ */
+int _isdigit(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	i = 0
+	while (i < len)
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: agument variables
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+		printf("Error\n");
+		exit(98);
+
+	if (!_isdigit(argv[1]))
+		printf("Error\n");
+		exit(98);
+	if (!_isdigit(argv[2]))
+		printf("Error\n");
+		exit(98);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,3 +3,4 @@
 2. 2-calloc.c allocates memory for an array using malloc.
 3. 3-array_range.c creates an array of integers.
 4. 100-realloc.c reallocates a block in memory using free.
+5. 101-mul.c multiplies two positive numbers.
