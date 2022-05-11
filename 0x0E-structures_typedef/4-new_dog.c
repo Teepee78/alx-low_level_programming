c7280ebe4ac2175e7c6ea873009f71ded555639b
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-11 22:43:30 +0100

    Update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,63 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to a new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	/* declare variables */
+	unsigned int nl, ol;
+	dog_t *dog;
+
+	/* if name of dog or name of owner is null */
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	/* allocate space for dog */
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	/* get length of name and add null byte */
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+
+	/* allocate space for dog name */
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = name;
+
+	/* allocate space for dog age */
+	dog->age = malloc(sizeof(float));
+	dog->age = age;
+
+	/* get length of owner and add null byte */
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+
+	/* allocate space for owner name */
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = owner;
+	return (dog);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,5 @@
 0. dog.h defines a new struct
 1. 1-init_dog.c initializes a variable of type dog
 2. 2-print_dog.c prints a structure
+3. dog.h defines a new name for struct dog
+4. 4-new_dog.c creates a new dog
