1893148da4088659abc08d8730919c09bee797de
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-11 21:23:37 +0100

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,11 +7,17 @@
  * @age: age of dog
  * @owner: name of owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+/**
+ *void init_dog(struct dog *d, char *name, float age, char *owner);
+ *void print_dog(struct dog *d);
+ *dog_t *newdog(char *name, float age, char *owner);
+ */
 
 #endif /* dog */
