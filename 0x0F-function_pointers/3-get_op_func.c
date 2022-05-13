bc36798d6c2e652c3b07adc96bdfe692ba9e53ff
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-13 21:43:47 +0100

    Update

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - gets correct function
+ * @s: operator
+ * @a: number 1
+ * @b: number 2
+ * Return: correct function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplys numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mods numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: a - b
+ */
+int op_add(int a, int b)
+{
+	return (a % b);
+}
