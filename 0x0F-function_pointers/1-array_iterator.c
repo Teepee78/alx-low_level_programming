de4ffd1824fdc4a9dd9511e347be88b944c78e06
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-13 20:00:45 +0100

    Update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	typedef int size_t;
+
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,10 @@
+#ifndef FUNC
+#define FUNC
+
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif /* FUNC */
