51149fbc07a35c8bcffabba5677e1c608ea3fb30
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-16 13:11:09 +0100

    Update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(nums, int));
 		if (i < n - 1)
-			printf("%s", separator);
+			{
+				if (!separator)
+				{
+					continue;
+				} else
+				{
+					printf("%s", separator);
+				}
+			}
 	}
 	va_end(nums);
 	printf("\n");
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			if (!separator)
 			{
-				printf("(nil)");
+				continue;
 			} else
 			{
 				printf("%s", separator);
