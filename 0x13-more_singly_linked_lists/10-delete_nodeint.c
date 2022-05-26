ff74a6e51afd0ac4a586a9ee50a8126c937e2044
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-26 13:00:40 +0100

    Update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < index - 1; i++)
 	{
-		if (!temp ||!(temp->next))
+		if (!temp || !(temp->next))
 			return (-1);
 		temp = temp->next;
 	}
