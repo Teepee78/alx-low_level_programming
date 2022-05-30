1cc870332dbcb5f21df833b85c02b0d39985665a
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-05-30 17:57:50 +0100

    Update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: binary number
+ * Return: unsigned int or NULL or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i, decimal = 0;
+
+	if (b == NULL)
+		return (NULL);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal = 2 * decimal + (b[i] - '0');
+	}
+
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef BITWISE
+#define BITWISE
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+unsigned int binary_to_uint(const char *b);
+
+#endif /* BITWISE */
