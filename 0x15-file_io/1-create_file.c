f44de6c51ddc48abc15e2dfc3d52c8f36dc89fce
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-06-01 12:54:05 +0100

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,11 +24,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		close(file);
 		return (1);
-	} else /* get the length of text_content */
-	{
-		while (text_content[len])
-			len++;
 	}
+
+	/* get the length of text_content */
+	while (text_content[len])
+		len++;
 
 
 	/* write into file */
