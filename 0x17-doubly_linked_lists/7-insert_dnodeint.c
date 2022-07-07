5c83ac9c4e6e58773153cb1ff9eea55502fb34d4
Toluwalase Phillips <eniolatoluwalase78@gmail.com>
2022-07-07 16:25:14 +0100

    Update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,7 +9,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int length = 0, i;
+	unsigned int i;
 	dlistint_t *list = *h, *new;
 
 	if (h == NULL)
