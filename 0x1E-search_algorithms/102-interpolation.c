c83c9b544ecca0a58b528abe2104e005553fa069
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-01 11:06:24 +0100

    Update

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,6 +1,37 @@
 #include "search_algos.h"
 
 /**
+ * interpolation_recursive - use recursion to perform interpolation search
+ * @array: pointer to first element in array to search
+ * @low: index of lower value
+ * @high: index of higher value
+ * @value: value to find
+ *
+ * Return: index of value or -1 if not found
+ */
+int interpolation_recursive(int *array, size_t low, size_t high, int value)
+{
+	size_t pos;
+
+	if (low >= high)
+		return (-1);
+	high--;
+	pos = low + (((double)(high - low) /
+				(array[high] - array[low])) * (value - array[low]));
+	if (pos > high)
+	{
+		printf("Value checked array[%lu] is out of range\n", pos);
+		return (-1);
+	}
+	printf("Value checked array[%lu] = [%i]\n", pos, array[pos]);
+	if (array[pos] == value)
+		return (pos);
+	if (value > array[pos])
+		return (interpolation_recursive(array, pos + 1, high, value));
+	return (interpolation_recursive(array, low, pos, value));
+}
+
+/**
  * interpolation_search - Interpolation Search Algorithm
  * @array: array of integers
  * @size: size of array
@@ -14,26 +45,5 @@ int interpolation_search(int *array, size_t size, int value)
 	if (array == NULL || size < 1)
 		return (-1);
 
-	while ((low < high) && (value >= array[low]))
-	{
-		pos = low + (((double)(high - low) /
-								(array[high] - array[low])) * (value - array[low]));
-
-		if (pos > high)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			return (-1);
-		}
-		printf("Value checked [%lu] = [%d]\n", pos, array[pos]);
-		if (array[pos] < value)
-			low = pos + 1;
-		else if (value < array[pos])
-			high = pos - 1;
-		else
-			return (pos);
-	}
-
-	if (value == array[low])
-		return (low);
-	return (-1);
+	return (interpolation_recursive(array, 0, size, value));
 }
